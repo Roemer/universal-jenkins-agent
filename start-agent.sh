c9bd75a50a131b3a86b07cb33a7ad8e7982258f2
Roman <r.baeriswyl@gmail.com>
2024-11-05 10:08:10 +0100

    Update start-agent.sh

--- start-agent.sh
+++ start-agent.sh
@@ -1,6 +1,4 @@
-
 AGENT_JAR_URL="${JENKINS_URL}/jnlpJars/agent.jar"
-JNLP_URL="${JENKINS_URL}/computer/${JENKINS_AGENT_NAME}/slave-agent.jnlp "
 
 curl --connect-timeout 20 --max-time 60 -o agent.jar $AGENT_JAR_URL
-java -jar agent.jar -jnlpUrl $JNLP_URL -secret $JENKINS_AGENT_SECRET -noReconnect -workDir /tmp
+java -jar agent.jar -url ${JENKINS_URL} -name ${JENKINS_AGENT_NAME} -secret $JENKINS_AGENT_SECRET -noReconnect -workDir /tmp
